56118afafc7a671f89a5ad083310e2d0e49b4760
Shinji Teramoto <shinji.toys@gmail.com>
2021-06-10 07:23:11 +0900

    add ch4

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 #![warn(clippy::all, clippy::pedantic)]
+mod document;
 mod editor;
+mod row;
 mod terminal;
+pub use document::Document;
 use editor::Editor;
 pub use editor::Position;
+pub use row::Row;
 pub use terminal::Terminal;
 
 fn main() {
+  dbg!("aaa".to_string().len());
+  dbg!("äää".to_string().len());
+  dbg!("y̆y̆y̆".to_string().len());
+  dbg!("❤❤❤".to_string().len());
   Editor::default().run();
 }
